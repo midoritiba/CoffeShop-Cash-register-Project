880652618a79d87ce1eeceec6d7e17de6f3a8fbb
midoritiba <mic.tiba@gmail.com>
2021-10-18 15:54:40 +0200

    ) added

--- products.js
+++ products.js
@@ -20,7 +20,7 @@ createProduct(1, "Cappuccino", "Coffee", 2.7);
 createProduct(2, "Caffe Americano", "Coffee", 1.95);
 createProduct(3, "Caffe Latte", "Coffee", 2.95);
 createProduct(4, "Croissant", "Snack", 2.25);
-createProduct(5, "Bagel", "Snack", 2.15;
+createProduct(5, "Bagel", "Snack", 2.15);
 createProduct(6, "Avocado Toast", "Snack", 2.55);
 createProduct(7, "Banana Pancakes", "Snack", 3.15);
 createProduct(8, "Chocolate Brownie", "Sweet", 2.55);
